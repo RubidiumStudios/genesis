a043309d6a67d080ec6b876fb31c5e1ff816259f
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-03-19 06:46:47 -0700

    Fix formatting in BOSH dryrun output

--- lib/Service/BOSH.pm
+++ lib/Service/BOSH.pm
@@ -167,7 +167,7 @@ sub dryrun_of {
 	$interactive = 1 if $execute && !defined($interactive);
 
 	dryrun(
-		"\nwould execute #G{%s} on #M{%s}, %s",
+		"\nwould execute #G{%s} on #M{%s}%s",
 		join(' ', map {$_ =~ /\s/ ? "'$_'" : $_} (humanize_path(scalar($self->command)), @cmd)),
 		$self->{alias} || $self->{host},
 		$execute ? ", resulting in$exec_msg:" : ""
