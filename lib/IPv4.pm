a800954776101a0ef77e59a3f4f61bc64db66cfa
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-08-20 14:32:36 -0700

    Fix IPv4.pm code style and formatting
    - Replace spaces with tabs for proper indentation
    - Remove unnecessary spacing in function calls
    - Simplify __strip_call_ref logic using ref() check
    - Fix spacing around eval blocks and conditionals
    - Maintain existing functionality while conforming to
    	project coding standards
    
    No API changes - existing IPv4 usage remains compatible.

--- lib/IPv4.pm
+++ lib/IPv4.pm
@@ -4,36 +4,29 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 
 sub new(@args) {
-    require IPv4::Range;
-    return IPv4::Range->new( __strip_call_ref(@args) )->simplify();
+	require IPv4::Range;
+	return IPv4::Range->new( __strip_call_ref(@args) )->simplify();
 }
 
 sub address(@args) {
-    require IPv4::Address;
-    return IPv4::Address->new( __strip_call_ref(@args) );
+	require IPv4::Address;
+	return IPv4::Address->new(__strip_call_ref(@args));
 }
 
 sub span(@args) {
-    require IPv4::Span;
-    return IPv4::Span->new( __strip_call_ref(@args) );
+	require IPv4::Span;
+	return IPv4::Span->new(__strip_call_ref(@args));
 }
 
 sub range(@args) {
-    require IPv4::Range;
-    return IPv4::Range->new( __strip_call_ref(@args) );
+	require IPv4::Range;
+	return IPv4::Range->new(__strip_call_ref(@args));
 }
 
 sub __strip_call_ref(@args) {
-
-    return () unless @args;
-
-    my $ref = shift @args;
-    if ( $ref eq __PACKAGE__ ) {
-        return @args;
-    }
-    else {
-        return ( $ref, @args );
-    }
+	return () unless @args;
+	shift @args if ref($args[0]) eq __PACKAGE__;
+	return @args;
 }
 
 sub __is_integer($value) {
@@ -43,16 +36,16 @@ sub __is_integer($value) {
 sub __autovivify($value) {
 	return $value->simplify if ref($value) =~ /^IPv4::(Address|Span|Range)$/;
 	my $obj = undef;
-	eval { $obj = IPv4->new($value) };
+	eval {$obj = IPv4->new($value)};
   my $err = $@;
-  if ($err) {
-    my $msg = sprintf(
-      "Invalid IPv4 literal or object %s",
-      ref($value) eq '' ? "'$value'" : ref($value).' type',
-    );
-    $msg .= ": $err" if $err !~ /Invalid IPv4 literal/;
-    die $msg;
-  }
+	if ($err) {
+		my $msg = sprintf(
+			"Invalid IPv4 literal or object %s",
+			ref($value) eq '' ? "'$value'" : ref($value).' type',
+		);
+		$msg .= ": $err" if $err !~ /Invalid IPv4 literal/;
+		die $msg;
+	}
 	return $obj;
 }
 
